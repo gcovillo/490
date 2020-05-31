a85a38563b4f1b29472715db13688e50026ab468
gcovillo <40395875+gcovillo@users.noreply.github.com>
2020-05-31 11:49:39 -0400

    Update autograder.php

--- autograder.php
+++ autograder.php
@@ -14,15 +14,23 @@ for ($x = 0; $x <= $examLength; $x++) {
   if ($funcCheck !== FALSE) { //means they put the correct function name
     $var_str = var_export($answer, true);
     file_put_contents('qwerty.py', $var_str); //NEED TO APPEND FUNCTION CALL AT END
-    $command = escapeshellcmd(python3 "test.py");
+    $command = escapeshellcmd(python3 "qwert.py");
     $output = shell_exec($command);
     } else {
     //subtract 5 points
-    
+    //replace function name
+    $lose = strstr($answer, ":");
+    $badFunc = str_replace($lose, " ", $answer);
+    $goodFunc = $funcName; //need to find a way to incorportate function name for forrect question into here
+    $py = $goodFunc.$lose;
+    $var_str = var_export($py, true);
+    file_put_contents('qwerty.py', $var_str);
+    $command = escapeshellcmd(python3, "qwerty.py");
+    $output = shell_exec($command);
     }
     
     
   }
 }
 //send results to back
-> 
+?> 
