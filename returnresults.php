2f07d33a2249ab1094d462d4591dea1c08ec3992
gcovillo <40395875+gcovillo@users.noreply.github.com>
2020-06-07 23:45:37 -0400

    Update returnresults.php

--- returnresults.php
+++ returnresults.php
@@ -1,14 +1,16 @@
 <?php
 // receive request from front 
-$teacherGradeRequest = $_POST['teachergradeRequest'];
+$username = $_POST['username']; 
+
+$examname = $_POST['examname'];
 
 
 //pass the request to the back
-$url = "https://web.njit.edu/~sk2773/teachergrade.php"; 
+$url = "https://web.njit.edu/~sk2773/getautograded.php"; 
 
 $ch = curl_init($url);
 
-$tgr = array('teacherGradeRequest' => $teacherGradeRequest);
+$tgr = array('student' => $username, 'exam' => $examname);
 $postString = http_build_query($tgr, '', '&');
 
 curl_setopt($ch, CURLOPT_POST, 1);
@@ -20,31 +22,6 @@ $result = curl_exec($ch);
 curl_close($ch);
 
 //  echo the results to front 
-echo $result
-
-//  receive new comments and grades from front
-
-$newGrade = $_POST['newGrade'];
-$comment = $_POST['comment'];
-$QID = $_POST['QID'];
-
-
-// post comments and new grades back to db
-$url = "https://web.njit.edu/~sk2773/?????";  //fix this
-
-$ch = curl_init($url);
-
-
-
-$updatedGrades = array('newGrade' => $newGrade, 'comment' => $comment, 'QID' => $QID);
-$postString = http_build_query($updatedGrades, '', '&');
-
-
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-$result = curl_exec($ch);
-curl_close($ch);
+echo $result;
 
 ?>
